9282b92b6382b0843b1749ff98e7c00a8cd0bf25
mlecabellec <mickael.lecabellec@gmail.com>
2015-02-14 15:31:37 +0100

    + OK, a true SurfaceView is required for webcam usage... SurfaceView added. Webcam OK. Have to be deeply tested.

--- app/src/main/java/com/booleanworks/ueshiba/WebHomeActivity.java
+++ app/src/main/java/com/booleanworks/ueshiba/WebHomeActivity.java
@@ -1,6 +1,8 @@
 package com.booleanworks.ueshiba;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -11,16 +13,24 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceView;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.Result;
+import com.google.zxing.common.GlobalHistogramBinarizer;
+
 import java.io.IOException;
 
 
-public class WebHomeActivity extends ActionBarActivity implements Camera.PictureCallback{
+public class WebHomeActivity extends ActionBarActivity implements Camera.PictureCallback {
 
     public SensorManager sensorManager;
     public SensorEventListener gyroListener;
@@ -32,7 +42,7 @@ public class WebHomeActivity extends ActionBarActivity implements Camera.Picture
         WebView webView = (WebView) this.findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.addJavascriptInterface(this, "activity");
-        webView.loadData("<h1 id='test' onclick='alert(activity.doCapture())'>default text 7</h1><script type='text/javascript'>alert(activity.testJs());activity.doCapture();</script>", "text/html", "UTF-8");
+        webView.loadData("<h1 id='test' onclick='alert(activity.doCapture())'>default text 10</h1><script type='text/javascript'>alert(activity.testJs());activity.doCapture();</script>", "text/html", "UTF-8");
 
         this.sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
@@ -97,15 +107,16 @@ public class WebHomeActivity extends ActionBarActivity implements Camera.Picture
         try {
             for (int ctCam = 0; ctCam < Camera.getNumberOfCameras(); ctCam++) {
                 Camera cCam = Camera.open(ctCam);
+                cCam.lock();
                 Camera.CameraInfo camInfo = new Camera.CameraInfo();
                 Camera.getCameraInfo(ctCam, camInfo);
                 if (camInfo.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
                     //cCam.enableShutterSound(true);
-                    SurfaceView dummySurfaceView = new SurfaceView(this);
+                    //SurfaceView dummySurfaceView = new SurfaceView(this);
 
-                    cCam.setPreviewDisplay(dummySurfaceView.getHolder());
+                    cCam.setPreviewDisplay(((SurfaceView)this.findViewById(R.id.webcamSurfaceView)).getHolder());
                     cCam.startPreview();
-                    cCam.takePicture(null,null,this);
+                    cCam.takePicture(null, null, this);
 
                 }
 
@@ -114,16 +125,40 @@ public class WebHomeActivity extends ActionBarActivity implements Camera.Picture
 
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (RuntimeException e) {
+            for (int ctCam = 0; ctCam < Camera.getNumberOfCameras(); ctCam++){
+
+            }
+            e.printStackTrace();
         }
-
-
     }
+
+
+
 
     @Override
     public void onPictureTaken(byte[] data, Camera camera) {
+        try {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
 
+            int[] colorData = new int[bitmap.getHeight() * bitmap.getWidth()];
+            bitmap.getPixels(colorData, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+            RGBLuminanceSource rgbLuminanceSource = new RGBLuminanceSource(bitmap.getWidth(), bitmap.getHeight(), colorData);
 
+            MultiFormatReader multiFormatReader = new MultiFormatReader();
+            GlobalHistogramBinarizer globalHistogramBinarizer = new GlobalHistogramBinarizer(rgbLuminanceSource);
+
+            Result decodeResult = multiFormatReader.decode(new BinaryBitmap(globalHistogramBinarizer));
+
+            Log.d("onPictureTaken", "text:" + decodeResult.getText());
+            ;
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+
+        camera.unlock();
         camera.release();
+
 
     }
 }
